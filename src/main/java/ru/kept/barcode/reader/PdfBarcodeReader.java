4ed3cce9428a217d4939fdd8f89e4edf4af26ce4
RUBY\alekseykomissarov <alekseykomissarov@kept.ru>
2023-12-10 03:22:06 +0300

    Noise is removed

--- src/main/java/ru/kept/barcode/reader/PdfBarcodeReader.java
+++ src/main/java/ru/kept/barcode/reader/PdfBarcodeReader.java
@@ -1,6 +1,7 @@
 package ru.kept.barcode.reader;
 
 import com.google.zxing.*;
+import lombok.experimental.UtilityClass;
 import lombok.extern.log4j.Log4j2;
 import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.io.RandomAccessReadBufferedFile;
@@ -12,16 +13,21 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.EnumMap;
 import java.util.List;
+import java.nio.file.Files;
 
 @Log4j2
-public class PdfBarcodeReader {
+@UtilityClass
+class PdfBarcodeReader {
 
-    public static List<BarcodeInformation> readBarcodeFromPdf(MultipartFile multipartFile) throws IOException {
+    public static List<BarcodeInformation> readBarcodeFromPdf(MultipartFile multipartFile) throws IOException, RuntimeException {
 
         List<File> filesForDeleting = new ArrayList<>();
 
@@ -38,8 +44,13 @@ public class PdfBarcodeReader {
 
             for (int page = 0; page < document.getNumberOfPages(); ++page) {
                 BufferedImage image = pdfRenderer.renderImageWithDPI(page, 300);
+                double contrastFactor = 1.5;
+                image = enhanceContrast(image, contrastFactor);
+                image = reduceNoise(image);
+                toGrayscale(image);
+
                 File outputfile = new File(tmpdir + "\\_page" + page + ".png");
-                filesForDeleting.add(pdfTmpFile);
+                filesForDeleting.add(outputfile);
                 ImageIO.write(image, "png", outputfile);
                 String barcodeData = readBarcodeFromFile(outputfile.getAbsolutePath());
 
@@ -51,9 +62,9 @@ public class PdfBarcodeReader {
             throw new RuntimeException(e);
         }finally {
             for (File file: filesForDeleting) {
-                var isDeleted = file.delete();
-                if (isDeleted)
-                    log.info("File " + file.getName() + " is deleted from tmp");
+
+                Files.deleteIfExists(file.toPath());
+
             }
 
         }
@@ -61,26 +72,87 @@ public class PdfBarcodeReader {
         return listOfBarcodes;
     }
 
-    public static String readBarcodeFromFile(String filePath) throws IOException, NotFoundException {
+    public static String readBarcodeFromFile(String filePath) {
+
         File barcodeFile = new File(filePath);
+        try {
 
-        if (!barcodeFile.exists()) {
-            throw new IOException("File not found: " + filePath);
+            BufferedImage bufferedImage = ImageIO.read(barcodeFile);
+            LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+            BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+            final EnumMap<DecodeHintType, Object> hints = new EnumMap<>(DecodeHintType.class);
+            hints.put(DecodeHintType.TRY_HARDER, true);
+            return new MultiFormatReader().decode(bitmap, hints).getText();
+
+        }catch (IOException | NotFoundException e) {
+            return "notFound";
         }
 
-        BufferedImage bufferedImage = ImageIO.read(barcodeFile);
+    }
 
-        if (bufferedImage == null) {
-            throw new IOException("Unable to read image file: " + filePath);
+    public static BufferedImage enhanceContrast(BufferedImage originalImage, double contrastFactor) {
+
+        int width = originalImage.getWidth();
+        int height = originalImage.getHeight();
+
+        BufferedImage resultImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int rgb = originalImage.getRGB(x, y);
+
+                int alpha = (rgb >> 24) & 0xFF;
+                int red = (rgb >> 16) & 0xFF;
+                int green = (rgb >> 8) & 0xFF;
+                int blue = rgb & 0xFF;
+
+                // Увеличение контрастности
+                red = (int) ((red - 128) * contrastFactor + 128);
+                green = (int) ((green - 128) * contrastFactor + 128);
+                blue = (int) ((blue - 128) * contrastFactor + 128);
+
+                // Ограничение значений до диапазона [0, 255]
+                red = Math.min(Math.max(red, 0), 255);
+                green = Math.min(Math.max(green, 0), 255);
+                blue = Math.min(Math.max(blue, 0), 255);
+
+                int newRGB = (alpha << 24) | (red << 16) | (green << 8) | blue;
+                resultImage.setRGB(x, y, newRGB);
+            }
         }
 
-        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
-        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+        return resultImage;
+    }
 
-        final HashMap<DecodeHintType, Object> hints = new HashMap<>();
-        hints.put(DecodeHintType.TRY_HARDER, true);
+    public static BufferedImage reduceNoise(BufferedImage originalImage) {
+        float[] matrix = {
+                1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f,
+                1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f,
+                1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f
+        };
+        BufferedImageOp op = new ConvolveOp(new Kernel(3, 3, matrix), ConvolveOp.EDGE_NO_OP, null);
+        return op.filter(originalImage, null);
+    }
 
-        return new MultiFormatReader().decode(bitmap, hints).getText();
+    private static void toGrayscale(BufferedImage image) {
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+
+                int rgb = image.getRGB(i, j);
+
+                int red = (rgb >> 16) & 0xFF;
+                int green = (rgb >> 8) & 0xFF;
+                int blue = (rgb) & 0xFF;
+
+                int gray = (red + green + blue) / 3;
+
+                image.setRGB(i, j, (gray << 16) | (gray << 8) | gray);
+            }
+        }
 
     }
 
